e2665403841c9a117494280416ee32d5d59b8928
Pablo Winant <pablo.winant@gmail.com>
2011-12-12 23:33:02 +0100

    Small convention change in F_rbc

--- F_rbc.m
+++ F_rbc.m
@@ -4,10 +4,10 @@ function out=F_rbc(s,x,e,snext,xnext,model);
 %model=rbc_matlab('model',none,none,none,none,none,none,none); 
 
 %rbc_matlab(flag,s,x,z,e,snext,xnext,p,out);
-h=rbc_matlab('h',s,x,[],e,snext,xnext,model.params);
+z=rbc_matlab('h',s,x,[],e,snext,xnext,model.params);
 
 %f 'rond' h
-f=rbc_matlab('f',s,x,h,e,[],[],model.params);
+f=rbc_matlab('f',s,x,z,e,[],[],model.params);
 
 out=f;
 end
