8e3cd8b8d47cc80fa68b76c56c4748144c4b8631
Pablo Winant <pablo.winant@gmail.com>
2011-12-13 18:25:48 +0100

    Added derivative computation to F_rbc and G_rbc.

--- F_rbc.m
+++ F_rbc.m
@@ -1,12 +1,16 @@
-function out=F_rbc(s,x,e,snext,xnext,model)
+function [F,F_s,F_x,F_S,F_X] =F_rbc(s,x,e,snext,xnext,model)
 
-
-
-
-z=rbc_matlab('h',s,x,[],e,snext,xnext,model.params);
-
-%f 'rond' h
-out=rbc_matlab('f',s,x,z,e,[],[],model.params);
-
+    if nargout == 1
+        z=rbc_matlab('h',s,x,[],e,snext,xnext,model.params);
+        F=rbc_matlab('f',s,x,z,e,[],[],model.params);
+    else
+        [z, z_s, z_x, z_S, z_X]=rbc_matlab('h',s,x,[],e,snext,xnext,model.params);
+        [f,f_s,f_x,f_z] = rbc_matlab('f',s,x,z,[],[],[],model.params);
+        F = f;
+        F_s = f_s + arraymult(f_z, z_s);
+        F_x = f_x + arraymult(f_z, z_x);
+        F_S = arraymult(f_z, z_S);
+        F_X = arraymult(f_z, z_X);
+    end
 
 end
--- G_rbc.m
+++ G_rbc.m
@@ -1,10 +1,15 @@
-function out=G_rbc(s,x,e,model);
+function [G, G_s, G_x, G_e] = G_rbc(s,x,e,model);
 % 
 %
 % none=[];
 % model=rbc_matlab('model',none,none,none,none,none,none,none); 
 
 %rbc_matlab(flag,s,x,z,e,snext,xnext,p,out);
-out=rbc_matlab('g',s,x,[],e,[],[],model.params);
+if nargout == 1
+    G = rbc_matlab('g',s,x,[],e,[],[],model.params);
+else
+    [G, G_s, G_x] = rbc_matlab('g',s,x,[],e,[],[],model.params);
+end
+
 
 end
